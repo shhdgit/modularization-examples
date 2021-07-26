76e7eed4dd69ad942557dd917158388b6c145d85
Suhaha <jklopsdfw@gmail.com>
2021-07-26 16:05:50 +0800

    init gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: `业务逻辑拆分模式`,
+    description: `业务逻辑拆分模式`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-gardenx`,
+      options: {
+        rootNote: "/README",
+        contentPath: `${__dirname}`,
+        parseWikiLinks: true,
+      },
+    },
+  ],
+};
